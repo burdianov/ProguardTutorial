0fac7ec70d0a57643bcbfacff87ea5c812f75dfb
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-04-10 22:19:36 +0400

    Update Gradle file

--- app/src/main/java/com/crackncrunch/proguardtutorial/MainActivity.java
+++ app/src/main/java/com/crackncrunch/proguardtutorial/MainActivity.java
@@ -1,7 +1,10 @@
 package com.crackncrunch.proguardtutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, new SomeClass("Hello").getSomeString(), Toast
+                .LENGTH_SHORT).show();
+        try {
+            Field someString = SomeClass.class.getDeclaredField("someString");
+            Toast.makeText(this, someString.getName(), Toast.LENGTH_SHORT).show();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/crackncrunch/proguardtutorial/SomeClass.java
@@ -0,0 +1,18 @@
+package com.crackncrunch.proguardtutorial;
+
+
+public class SomeClass {
+    private String someString;
+
+    public SomeClass(String someString) {
+        this.someString = someString;
+    }
+
+    public String getSomeString() {
+        return someString;
+    }
+
+    public void setSomeString(String someString) {
+        this.someString = someString;
+    }
+}
